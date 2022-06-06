401f9d28a1222ba2a219c968d0846d3b626d2126
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-06-06 17:17:21 +0000

    p2 atv semana

---
+++ atvSemana.java
@@ -0,0 +1,13 @@
+public class atvSemana{
+    public static void main(String[]args){
+    int segundaSemana= 5;
+    int terceirsemana= 2;
+    int quartaSemana = 7;
+
+    
+    System.out.println("Segunda Semana - "+segundaSemana);
+    System.out.println("Terceira Semana - "+terceirsemana);
+    System.out.println("QuartaSsemana - "+quartaSemana);
+   // 
+}
+}
